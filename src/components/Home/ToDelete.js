66266df4d75ce67b17a54e9e0586c3e604301196
Evgeniy Budaev <budaev.e@gmail.com>
2021-12-22 07:35:48 +0300

    Login, update Redux and Router, ui-kit, fonts, colors, shadows

--- src/components/Home/ToDelete.js
+++ src/components/Home/ToDelete.js
@@ -1,15 +1,8 @@
 import React from "react";
-import cn from "classnames";
-import classes from "./ToDelete.module.scss";
 
 function ToDelete() {
     return (
         <div className="to-delete">
-            <h1 className={cn(classes.Title)}>Hello World</h1>
-            <div className={cn(classes.text)}>
-                Этот компонент страницы вы можете удалить сразу. Он нужен, чтобы
-                поприветствовать вас и вдохновить на коллективную работу.
-            </div>
             <h2 className="to-delete__title toDeleteTitle">Привет!</h2>
             <p className="to-delete__paragraph">
                 Этот компонент страницы вы можете удалить сразу. Он нужен, чтобы
